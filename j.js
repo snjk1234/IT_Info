f1ce754f0c2d65327091e61761ad374afc5bb608
omar <108716931+snjk1234@users.noreply.github.com>
2025-02-09 19:22:11 +0300

    Update j.js
    SAVEING DATA

--- j.js
+++ j.js
@@ -113,6 +113,6 @@ function clearInputs() {
     PRINTERInput.value = '';
     PRINTER_VERInput.value = '';
 }
-
 // Initial render
 renderTable();
+window.addEventListener('DOMContentLoaded', renderTable);
